6dd328c074ec49e07aef213113942f2c102d196b
Alfrendo Silalahi <alfrendos72@gmail.com>
2023-10-31 15:36:38 +0700

    create simple module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import http from 'http';
 import url from 'url';
+import replaceTemplate from './modules/replace-template.js';
 
 /////////////////////////////////
 // FILES
@@ -19,22 +20,6 @@ const dataObj = JSON.parse(data);
 
 /////////////////////////////////
 // SERVER
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  }
-
-  return output;
-};
 
 const server = http.createServer((req, res) => {
   const { query, pathname: pathName } = url.parse(req.url, true);
